43e64fed155f777805284c0b8c0447ca59d1a005
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-07-08 02:05:47 +0200

    03_suiteDeFibonacci.js

--- eau03.js
+++ eau03.js
@@ -1,18 +1,20 @@
 function fibonacci(n) {
-  let a = 0, b = 1, c;
-  if (n === 0) return a;
-  for (let i = 2; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
+    let a = 0, b = 1, c;
+    if (n === 0) return a;
+    for (let i = 2; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return b;
   }
-  return b;
-}
-const n = parseInt(process.argv[2]);
-
-if(n < 0){
-    console.log("-1");
-    process.exit(1);
-}
-const result = fibonacci(13);
-console.log(result);
+  
+  const n = parseInt(process.argv[2]);
+  
+  if(n < 0){
+      console.log("-1");
+      process.exit(1);
+  }
+  
+  const result = fibonacci(13);
+  console.log(result);
