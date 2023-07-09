07edc3ebb8b97a4eaeb7a2f529a7d4047794aa4a
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-07-09 13:13:25 +0200

    08_chiffresOnly.js

---
+++ eau08.js
@@ -0,0 +1,15 @@
+if(process.argv.length < 2){
+    console.log("erreur");
+    process.exit(1)
+}
+
+const nbr = process.argv;
+for(let i = 0; i < process.argv; i++){
+    console.log(nbr);
+}
+
+if(isNaN !== process.argv){
+    console.log("false");
+} else {
+    console.log("true");
+}
