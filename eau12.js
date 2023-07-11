43c419246f5322e02274b1d0e0a3849ffcc987c3
Jeremy-dev94 <jgaudry1294@gmail.com>
2023-07-11 21:48:41 +0200

    12_triA_Bulle.js

--- eau12.js
+++ eau12.js
@@ -1,7 +1,12 @@
-const array = [ 1, 2, 3, 4, 5, 6, 7,];
+const array = [ 1, 2, 3, 4, 5, 6, 7];
 
-for let i = 0; i < array.length; i++) {
-    for j = i + 1; j < array.length; j++) {
-        console.log(array[i] + array[j]);
+for (let i = array.length - 1; i < 0; i = i - 1) {
+    for (let j = 0; j < i; j = j + 1) {
+    if (array[j + 1] < array[i + 1]){
+        let temp = array[j + 1];
+        array[j + 1] = array[i + 1];
+        array[i + 1] = temp;
     }
+  }
 }
+console.log(array);
